f58a25e519be5b02edeaf86b47f14c41ec0da350
Seyi Ogunniran <seyi.no@greenafrica.com>
2025-05-22 11:19:33 +0100

    updated backend route

--- modules/sgs/main.tf
+++ modules/sgs/main.tf
@@ -49,6 +49,13 @@ resource "aws_security_group" "allow_backend" {
     ]
   }
 
+ingress {
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["10.1.160.0/20"]  # Or just the bastion's private IP as /32
+}
+
   egress {
     from_port   = 0
     to_port     = 0
